9afd4f790e3565710a0717449407b6da9367e89b
Fernando Vicente <ffernandovicente@gmail.com>
2022-12-07 23:54:15 -0300

    initial

---
+++ models/userModel.go
@@ -0,0 +1,15 @@
+package models
+
+// type User struct {
+// 	ID	primitive.Ob
+// 	First_name
+// 	Last_name
+// 	Password
+// 	Email
+// 	Phone
+// 	Token
+// 	User_type
+// 	Refresh_token
+// 	Created_at
+// 	Updated_at
+// }
